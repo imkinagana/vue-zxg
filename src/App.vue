6e533f5e6340fbb2eb8c54794bfac6821b556304
imkinagana <imkinagana@protonmail.com>
2021-07-20 22:49:13 +0200

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  font-family: 'Open Sans', 'Lato', sans-serif;
+}
+
+#app {
+  width: 100vw;
+  min-height: 100vh;
+  height: auto;
+  font-family: 'Open Sans', 'Lato', sans-serif;
+  background-color: rgba(21, 21, 21, 1.0);
+  color: rgba(255, 255, 255, 1.0);
+}
+</style>
